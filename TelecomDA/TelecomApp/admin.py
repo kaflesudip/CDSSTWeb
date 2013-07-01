e5144e6a14e31078ed4fdf94519ed6b9706e8868
sudip kafle <soodip.kafle@gmail.com>
2013-07-01 11:47:32 +0545

    added models and admin

---
+++ TelecomDA/TelecomApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import *
+
+admin.site.register(HomeSlider)
+admin.site.register(HomeContent)
+admin.site.register(Overview)
+admin.site.register(Screenshot)
+admin.site.register(Team)
